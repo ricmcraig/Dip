965b21dab23c446a1436f70759548e9224fb74ed
Ric Craig <ricmcraig@gmail.com>
2012-01-05 16:58:39 +0000

    The map package holds unchanging details of the board setup.

---
+++ Dip/src/java/net/craigrm/dip/map/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides an immutable representation of a Diplomacy map. 
+ * <p>
+ * To instantiate a map, the client must provide an {@link net.craigrm.dip.map.IMapper} 
+ * implementation which parses a map definition and makes available the set of 
+ * {@link net.craigrm.dip.map.Province} objects that together make up the map.
+ */
+package net.craigrm.dip.map;
