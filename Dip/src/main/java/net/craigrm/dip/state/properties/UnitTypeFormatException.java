3a16452bf3399a051c7bfb26ac561a681f492666
Ric Craig <ricmcraig@gmail.com>
2012-01-25 16:18:52 +0000

    Improved state exception reporting.

---
+++ Dip/src/main/java/net/craigrm/dip/state/properties/UnitTypeFormatException.java
@@ -0,0 +1,23 @@
+package net.craigrm.dip.state.properties;
+
+public class UnitTypeFormatException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = 0L;
+
+	public UnitTypeFormatException() {
+		super();
+	}
+
+	public UnitTypeFormatException(String s) {
+		super(s);
+	}
+
+	public UnitTypeFormatException(Throwable cause) {
+		super(cause);
+	}
+
+	public UnitTypeFormatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
