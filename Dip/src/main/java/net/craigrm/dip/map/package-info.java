dd4d4a7ef9a094d7016aabc5110aba5523419fe4
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:26:54 +0000

    Updated as part of interface renaming

--- Dip/src/main/java/net/craigrm/dip/map/package-info.java
+++ Dip/src/main/java/net/craigrm/dip/map/package-info.java
@@ -3,7 +3,7 @@
  * <p>
  * The map is represented by {@link net.craigrm.dip.map.DipMap}
  * <p>
- * To instantiate a map, the client provides a {@link net.craigrm.dip.map.IMapper} 
+ * To instantiate a map, the client provides a {@link net.craigrm.dip.map.IMapDataSource} 
  * implementation which parses a map definition and makes available the set of 
  * {@link net.craigrm.dip.map.Province} objects that together make up the map.
  * <p>
