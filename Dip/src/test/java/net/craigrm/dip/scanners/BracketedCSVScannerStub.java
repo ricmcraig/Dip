4eb5c38907f36444acbf0c87cbbbee704166c10f
Ric Craig <ricmcraig@gmail.com>
2012-03-12 16:07:00 +0000

    Updated to reflect move of IBracketedCSVScanner interface

--- Dip/src/test/java/net/craigrm/dip/scanners/BracketedCSVScannerStub.java
+++ Dip/src/test/java/net/craigrm/dip/scanners/BracketedCSVScannerStub.java
@@ -1,11 +1,9 @@
 package net.craigrm.dip.scanners;
 
-import net.craigrm.dip.map.IBracketedCSVScanner;
-
-public class BracketedCSVScannerStub implements IBracketedCSVScanner {
+public class BracketedCSVScannerStub  implements IBracketedCSVScanner{
 
 	private String[] identifierNames;
-	public BracketedCSVScannerStub(String... identifierNames){
+	public BracketedCSVScannerStub(String... identifierNames) {
 		this.identifierNames = identifierNames;
 	}
 
