7d02a0e612a3780fd619933158318ef63de4d062
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:53:49 +0000

    Changed to ensure only a single instance is constructed. Instance is exposed through a class method
    Enhanced interface to provide info on units

--- Dip/src/main/java/net/craigrm/dip/state/TurnState.java
+++ Dip/src/main/java/net/craigrm/dip/state/TurnState.java
@@ -3,7 +3,8 @@ package net.craigrm.dip.state;
 import java.util.Collections;
 import java.util.Set;
 
-import net.craigrm.dip.gameturn.IPositionParser;
+import net.craigrm.dip.gameturn.IPositionDataSource;
+import net.craigrm.dip.map.properties.Powers;
 
 /**
  * 
@@ -12,26 +13,68 @@ import net.craigrm.dip.gameturn.IPositionParser;
  */
 public class TurnState {
 
+	private static TurnState turnState;
 	private final TurnIdentifier turnID;
 	private final Set<Unit> units;
 	private final Set<Control> control;
 	
-	public TurnState(IPositionParser pp) {
-		this.turnID = new TurnIdentifier(pp.getYear(), pp.getSeason());
+	/**
+	 * Constructs a new TurnState instance from a PositionParser if there 
+	 * is no current TurnState instance. The PositionParser is responsible 
+	 * for extracting the unit positions from a  position definition of 
+	 * whatever form.  
+	 * 
+	 * @param pp an {@link net.craigrm.dip.state.IPositionDataSource IPositionParser}
+	 *  implementation that extracts unit position data from a position definition.
+	 */
+	public static void setTurnState(IPositionDataSource pp) {
+		if (turnState == null) {
+			turnState = new TurnState(pp);
+		}
+	}
+
+	/**
+	 * Provides the sole instance of TurnState created by 
+	 * {@link net.craigrm.dip.state.TurnState#setTurnState(IPositionDataSource) setTurnState(IPositionParser)}
+	 * @return the TurnState instance
+	 * @throws IllegalStateException if TurnState has not been instantiated
+	 */
+	public static TurnState getState() {
+		if (turnState == null) {
+			throw new IllegalStateException();
+		}
+		return turnState;
+	}
+	
+	private TurnState(IPositionDataSource pp) {
+		this.turnID = pp.getTurnID();
 		this.units = pp.getUnits();
 		this.control = pp.getControl();
 	}
 
-	public TurnIdentifier getTurnIdentifier(){
+	public TurnIdentifier getTurnIdentifier() {
 		return turnID;
 	}
 	
-	public Set<Unit> getUnits(){
+	public Set<Unit> getUnits() {
 		return Collections.unmodifiableSet(units);
 	}
 
-	public Set<Control> getControl(){
+	public Set<Control> getControl() {
 		return Collections.unmodifiableSet(control);
 	}
 
+	public boolean isAnyUnitPresent(Unit u) {
+		for (Powers p:Powers.values()) {
+			Unit any = new Unit( u.getCurrentPosition(), p, u.getUnitType()); 
+			if (units.contains(any)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isOwnedUnitPresent(Unit u) {
+		return units.contains(u);
+	}
 }
