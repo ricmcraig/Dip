3c976ee8164621e4627bdbc71253246aa9e28daa
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:37:36 +0000

    Flesh out SupportingOrder class
    Updated to reflect work on Order class

--- Dip/src/main/java/net/craigrm/dip/orders/SupportingOrder.java
+++ Dip/src/main/java/net/craigrm/dip/orders/SupportingOrder.java
@@ -1,18 +1,26 @@
 package net.craigrm.dip.orders;
 
-import net.craigrm.dip.map.Identifier;
+import net.craigrm.dip.map.ProvinceIdentifier;
 import net.craigrm.dip.orders.properties.OrderType;
 import net.craigrm.dip.state.Unit;
 
 public class SupportingOrder extends Order {
 
-	Unit supportedUnit;
-	OrderType supportedOrderType;
+	private final Unit supportedUnit;
+	private final OrderType supportedOrderType;
 	
-	public SupportingOrder(Unit unit, OrderType orderType, Unit supportedUnit, OrderType supportedOrderType, Identifier destination){
-		super(unit, orderType, destination);
+	public SupportingOrder(String orderText, boolean wellFormed, Unit unit, OrderType orderType, Unit supportedUnit, OrderType supportedOrderType, ProvinceIdentifier destination) {
+		super(orderText, wellFormed, unit, orderType, destination);
 		this.supportedUnit = supportedUnit;
 		this.supportedOrderType = supportedOrderType;
-		
 	}
+
+	public Unit getSupportedUnit() {
+		return supportedUnit;
+	}
+
+	public OrderType getSupportedOrderType() {
+		return supportedOrderType;
+	}
+	
 }
