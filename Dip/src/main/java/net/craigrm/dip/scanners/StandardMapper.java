f7112073e305a35203091dc9e41c419f6a1748ac
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:20:32 +0000

    Refactored for better exception handling and for removal of Aliases and Neighbours classes

--- Dip/src/main/java/net/craigrm/dip/scanners/StandardMapper.java
+++ Dip/src/main/java/net/craigrm/dip/scanners/StandardMapper.java
@@ -8,18 +8,16 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-import net.craigrm.dip.map.Aliases;
-import net.craigrm.dip.map.DuplicateIdentifierException;
-import net.craigrm.dip.map.IMapper;
-import net.craigrm.dip.map.Identifier;
+import net.craigrm.dip.map.DuplicateProvinceIdentifierException;
+import net.craigrm.dip.map.IMapDataSource;
+import net.craigrm.dip.map.ProvinceIdentifier;
 import net.craigrm.dip.map.MapDefinitionException;
-import net.craigrm.dip.map.Neighbours;
 import net.craigrm.dip.map.Province;
-import net.craigrm.dip.map.properties.Powers;
+import net.craigrm.dip.map.properties.Owner;
 import net.craigrm.dip.map.properties.Supply;
 import net.craigrm.dip.map.properties.Terrains;
 
-public class StandardMapper implements IMapper{
+public class StandardMapper implements IMapDataSource{
 
 	private static final String OPTIONAL_WHITESPACE_REGEX = "\\s*";
 	private static final String IDENTIFIER_MAIN_PART_REGEX = "[a-zA-Z]{3}";
@@ -31,15 +29,14 @@ public class StandardMapper implements IMapper{
 	private File mapFile;
 	private Set<Province> provinces = new HashSet<Province>();
 	
-	public StandardMapper(File mapFile){
+	public StandardMapper(File mapFile) {
 		this.mapFile = mapFile;
 		checkFile();
 		parseMapDefinition();
 	}
 
-	public StandardMapper(String mapFileName){
-		if (mapFileName == null)
-		{
+	public StandardMapper(String mapFileName) {
+		if (mapFileName == null) {
 			throw new IllegalArgumentException("Map file name not specified.");
 		}
 		this.mapFile = new File(mapFileName);
@@ -47,80 +44,117 @@ public class StandardMapper implements IMapper{
 		parseMapDefinition();
 	}
 	
-	public Set<Province> getProvinces(){
+	public Set<Province> getProvinces() {
 		return provinces;
 	}
 
-	private void parseMapDefinition(){
+	private void parseMapDefinition() {
+		FileReader fr = null;
 		Set<Province> provisionalProvinces = new HashSet<Province>();
-		Set<Identifier> seaProvincesIds = new HashSet<Identifier>();
+		Set<ProvinceIdentifier> seaProvincesIds = new HashSet<ProvinceIdentifier>();
 		Scanner lineScanner = null;
 		String line = null; 
 		int lineNo = 0;
+
+		try {
+			 fr = new FileReader(mapFile);
+		}
+		catch (FileNotFoundException fnfe) {
+			throw new IllegalArgumentException("Position file " + mapFile.getAbsolutePath() + " cannot be found.");
+		} 
 		
+		BufferedReader br = new BufferedReader(fr);
 		// First pass: read the file and create set of provisional provinces and capture list of sea province names
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(mapFile));
-			while ((line = br.readLine()) != null){
+		try {
+			while ((line = br.readLine()) != null) {
 				lineNo++;
 				lineScanner = new Scanner(line);
 				lineScanner.useDelimiter(",");
 				
-				if (!lineScanner.hasNext())
+				if (!lineScanner.hasNext()) {
 					throw new NoSuchMapElementException("Identifier");
-				Identifier id = new Identifier(lineScanner.next());
+				}
+				ProvinceIdentifier id = new ProvinceIdentifier(lineScanner.next());
 				
-				if (!lineScanner.hasNext())
+				if (!lineScanner.hasNext()) {
 					throw new NoSuchMapElementException("Terrain");
+				}
 				Terrains terrain = Terrains.getTerrain(lineScanner.next());
 				
-				if (!lineScanner.hasNext())
+				if (!lineScanner.hasNext()) {
 					throw new NoSuchMapElementException("Suppply");
+				}
 				Supply supply = Supply.getSupply(lineScanner.next());
 				
-				if (!lineScanner.hasNext())
+				if (!lineScanner.hasNext()) {
 					throw new NoSuchMapElementException("Power");
-				Powers owner = Powers.getPower(lineScanner.next());
+				}
+				Owner owner = new Owner(lineScanner.next());
 				
-				if (!lineScanner.hasNext())
+				if (!lineScanner.hasNext()) {
 					throw new NoSuchMapElementException("Full Name");
+				}
 				String fullName = lineScanner.next();
 				
 				String aliasesString = getIdentifierList(lineScanner);
-				if (aliasesString == null)
+				if (aliasesString == null) {
 					throw new NoSuchMapElementException("Aliases");
-				Aliases aliases = new Aliases(new BracketedCSVScanner(aliasesString));
+				}
+				String[] aliaseIDs = new BracketedCSVScanner(aliasesString).getElements();
+				Set<ProvinceIdentifier> aliases = new HashSet<ProvinceIdentifier>(); 
+				for(String alias:aliaseIDs) {
+					ProvinceIdentifier aliasID = new ProvinceIdentifier(alias);
+					if (!aliases.add(aliasID)) {
+						throw new DuplicateProvinceIdentifierException(aliasID.getID());
+					}
+				}
 				
 				String neighboursString = getIdentifierList(lineScanner);
-				if (neighboursString == null)
+				if (neighboursString == null) {
 					throw new NoSuchMapElementException("Neighbours");
-				Neighbours neighbours = new Neighbours(new BracketedCSVScanner(neighboursString));
+				}
+				String[] neighbourIDs = new BracketedCSVScanner(neighboursString).getElements();
+				Set<ProvinceIdentifier> neighbours =  new HashSet<ProvinceIdentifier>();
+				for(String neighbour:neighbourIDs) {
+					ProvinceIdentifier neighbourID = new ProvinceIdentifier(neighbour);
+					if (!neighbours.add(neighbourID)) {
+						throw new DuplicateProvinceIdentifierException(neighbourID.getID());
+					}
+				}
 				
-				if (!provisionalProvinces.add(new Province(id, terrain, supply, owner, fullName, aliases, neighbours)))
-					throw new DuplicateIdentifierException(id.getID());
-				
+				if (!provisionalProvinces.add(new Province(id, terrain, supply, owner, fullName, aliases, neighbours))) {
+					throw new DuplicateProvinceIdentifierException(id.getID());
+				}
+
 				if (terrain == Terrains.SEA) {
 					seaProvincesIds.add(id);
 				}
 			}
 		}
-		catch (FileNotFoundException fnfe){
-			throw new MapDefinitionException("Map file cannot be found.", fnfe, mapFile.getAbsolutePath(), lineNo);
-		} 
-		catch (IOException ioe){
-			throw new MapDefinitionException("Map file cannot be read.", ioe, mapFile.getAbsolutePath(), lineNo);
+		catch (IOException ioe) {
+			throw new IllegalArgumentException("Map file " + mapFile.getAbsolutePath() + " cannot be read.");
 		}
-		catch (NoSuchMapElementException nsmee){
-			throw new MapDefinitionException("Map file has a missing element.", nsmee, mapFile.getAbsolutePath(), lineNo);
+		catch (NoSuchMapElementException nsmee) {
+			throw new MapDefinitionException(mapFile.getAbsolutePath(), lineNo, line,  nsmee.getMissingElementName() + " element", nsmee);
 		}
-		catch (DuplicateIdentifierException die){
-			throw new MapDefinitionException("Map file has a duplicate alias.", die, mapFile.getAbsolutePath(), lineNo);
+		catch (DuplicateProvinceIdentifierException die) {
+			throw new MapDefinitionException(mapFile.getAbsolutePath(), lineNo, line, "single " + die.getDuplicateIdentifier() + "identifier", die);
+		}
+		finally{
+			if (br != null) {
+				try {
+					br.close();
+				}
+				catch(IOException ioe) {
+					//TODO Log exception. Continue anyway: we read the file successfully
+				}
+			}
 		}
 		
 		// Second pass: adjust provisional inland provinces to coastal provinces if they have a neighbouring sea province
-		for(Province p: provisionalProvinces){
+		for(Province p: provisionalProvinces) {
 			Province adjustedProvince = null;
-			if(p.getType() == Terrains.INLAND && p.getNeighbours().containsAny(seaProvincesIds)){
+			if(p.getType() == Terrains.INLAND && containsAny(p.getNeighbours(), seaProvincesIds)) {
 				 adjustedProvince = new Province(p.getIdentifier(), Terrains.COAST, p.getSupply(), p.getOwner(), p.getFullName(), p.getAliases(), p.getNeighbours());}
 			else {
 				adjustedProvince = p;
@@ -130,8 +164,8 @@ public class StandardMapper implements IMapper{
 		
 	}
 	
-	private String getIdentifierList(Scanner lineScanner){
-		if(lineScanner.hasNext(EMPTY_IDENTIFIER_LIST_REGEX)){
+	private String getIdentifierList(Scanner lineScanner) {
+		if(lineScanner.hasNext(EMPTY_IDENTIFIER_LIST_REGEX)) {
 			return lineScanner.next(EMPTY_IDENTIFIER_LIST_REGEX);
 		}
 
@@ -143,7 +177,15 @@ public class StandardMapper implements IMapper{
 			throw new IllegalArgumentException("Map file not specified.");
 		}
 		
-		if (!mapFile.isFile())
+		if (!mapFile.isFile()) {
 			throw new IllegalArgumentException("Map file " + mapFile.getAbsolutePath() + " cannot be accessed.");
+		}
 	}
+	
+	private boolean containsAny(Set<ProvinceIdentifier> s1, Set<ProvinceIdentifier> s2) {
+		Set<ProvinceIdentifier> intersection = new HashSet<ProvinceIdentifier>(s1);
+		intersection.retainAll(s2);
+		return !intersection.isEmpty();
+	}
+
 }
