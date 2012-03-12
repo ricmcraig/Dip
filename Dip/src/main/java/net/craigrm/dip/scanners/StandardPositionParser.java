8783452fb4f1aa972bd1216234a9d6610114e646
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:22:21 +0000

    Refactored for better exception handling

--- Dip/src/main/java/net/craigrm/dip/scanners/StandardPositionParser.java
+++ Dip/src/main/java/net/craigrm/dip/scanners/StandardPositionParser.java
@@ -11,39 +11,38 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-import net.craigrm.dip.gameturn.IPositionParser;
-import net.craigrm.dip.map.Identifier;
+import net.craigrm.dip.gameturn.IPositionDataSource;
+import net.craigrm.dip.map.ProvinceIdentifier;
 import net.craigrm.dip.map.properties.Powers;
 import net.craigrm.dip.state.Control;
+import net.craigrm.dip.state.PositionDefinitionException;
+import net.craigrm.dip.state.TurnIdentifier;
+import net.craigrm.dip.state.TurnIdentifierFormatException;
 import net.craigrm.dip.state.Unit;
-import net.craigrm.dip.state.properties.Season;
 import net.craigrm.dip.state.properties.SeasonFormatException;
 import net.craigrm.dip.state.properties.UnitType;
-import net.craigrm.dip.state.properties.Year;
 import net.craigrm.dip.state.properties.YearFormatException;
 
-public class StandardPositionParser implements IPositionParser{
+public class StandardPositionParser implements IPositionDataSource{
 
-	private static final String POSITION_FILE_COMMENT_INDICATOR = "#";
-	private static final int TURN_YEAR_START = 0;
-	private static final int TURN_YEAR_END = 4;
-	private static final int TURN_SEASON_START = 4;
-	private static final int TURN_SEASON_END = 5;
+	private static final String COMMENT_PREFIX = "#";
+	private static final String TURN_PREFIX = "=";
+	private static final String WHITESPACE_REGEX =  "\\s*";
+	private static final String CONTROL_LINE_REGEX = ".*,.*";
+	private static final String POSITION_LINE_REGEX = ".*,.*,.*";
 
 	private File posFile;
 	private Set<Unit> units = new HashSet<Unit>();
 	private Set<Control> control = new HashSet<Control>();
-	private Year year = null;
-	private Season season = null;
+	private TurnIdentifier turnID = null;
 	
-	public StandardPositionParser(File posFile){
+	public StandardPositionParser(File posFile) {
 		this.posFile = posFile;
 		checkFile();
 	}
 
-	public StandardPositionParser(String posFileName){
-		if (posFileName == null)
-		{
+	public StandardPositionParser(String posFileName) {
+		if (posFileName == null) {
 			throw new IllegalArgumentException("Position file name not specified.");
 		}
 		File mapFile = new File(posFileName);
@@ -51,88 +50,117 @@ public class StandardPositionParser implements IPositionParser{
 		checkFile();
 	}
 	
-	public Set<Unit> getUnits(){
+	public Set<Unit> getUnits() {
 		return Collections.unmodifiableSet(units);
 	}
 
-	public Set<Control> getControl(){
+	public Set<Control> getControl() {
 		return Collections.unmodifiableSet(control);
 	}
 
-	public Year getYear(){
-		return year;
+	public TurnIdentifier getTurnID() {
+		return turnID;
 	}
 
-	public Season getSeason(){
-		return season;
-	}
-
-	public void parsePositionDefinition(){
+	public void parsePositionDefinition() {
+		FileReader fr = null;
 		Scanner lineScanner = null;
 		String line = null; 
 		int lineNo = 0;
 		
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(posFile));
-			// Get the turn identifier on the first non-comment line
-			while ((line = br.readLine()) != null){
-				lineNo++;
-				if (line.startsWith(POSITION_FILE_COMMENT_INDICATOR)){
-					continue;
-				}
-				year = new Year(line.substring(TURN_YEAR_START, TURN_YEAR_END));
-				season = Season.getSeason(line.substring(TURN_SEASON_START, TURN_SEASON_END));
-				break;
-			}
-			if (year == null || season == null){
-				throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + ". Could not find turn identifier");
-			}
-			
-			// Get the unit positions from subsequent non-comment lines
-			while ((line = br.readLine()) != null){
-				lineNo++;
-				if (line.startsWith(POSITION_FILE_COMMENT_INDICATOR)){
-					continue;
-				}
-				lineScanner = new Scanner(line);
-				lineScanner.useDelimiter(",");
-				if (line.matches(".*,.*,.*")) {
-					Identifier id = new Identifier(lineScanner.next());
-					Powers unitOwner = Powers.getPower(lineScanner.next());
-					UnitType unitType = UnitType.getType(lineScanner.next());
-					units.add(new Unit(id, unitOwner, unitType));
-				} else if (line.matches(".*,.*")) {
-					Identifier id = new Identifier(lineScanner.next());
-					Powers controller = Powers.getPower(lineScanner.next());
-					control.add(new Control(id, controller));
-					
-				} else {
-					throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + ". Line " + lineNo + " unexpected format.");
-				}
-			}
-			if (units.isEmpty()){
-				throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + ". Could not find unit positions.");
-			}
-			
-			if (control.isEmpty()){
-				throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + ". Could not find control.");
-			}
-			
+		try {
+			fr = new FileReader(posFile);
 		}
-		catch (FileNotFoundException fnfe){
+		catch (FileNotFoundException fnfe) {
 			throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + " cannot be found.");
 		}
-		catch (IOException ioe){
+		
+		BufferedReader br = new BufferedReader(fr);
+		try {
+			
+			// Get the turn identifier on the first non-comment line
+			while ((line = br.readLine()) != null) {
+				lineNo++;
+				// Skip comment line
+				if (line.startsWith(COMMENT_PREFIX)) {
+					continue;
+				}
+				
+				//Skip blank line
+				if (line.matches(WHITESPACE_REGEX)) {
+					continue;
+				}
+				
+				// Handle turn designation line
+				if (line.startsWith(TURN_PREFIX)) {
+					if (turnID != null) {
+						throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, "a single Turn designation");
+					}
+					try {
+						turnID = new TurnIdentifier(line.substring(TURN_PREFIX.length()));
+					}
+					catch (TurnIdentifierFormatException tife) {
+						throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, "a correctly formatted turn ID", tife);
+					}
+					catch (YearFormatException yfe) {
+						throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, "a valid turn year", yfe );
+					}
+					catch (SeasonFormatException sfe) {
+						throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, "a valid turn season", sfe );
+					}
+					continue;
+				}
+				
+				//Should be an position or control line: check we've got a turn
+				if (turnID == null) {
+					throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, "a turn designation before this point");
+				}
+				
+				//Handle position and control lines
+				lineScanner = new Scanner(line);
+				lineScanner.useDelimiter(",");
+				if (line.matches(POSITION_LINE_REGEX)) {
+					ProvinceIdentifier id = new ProvinceIdentifier(lineScanner.next());
+					Powers unitOwner = Powers.getPowerFromID(lineScanner.next());
+					UnitType unitType = UnitType.getType(lineScanner.next());
+					units.add(new Unit(id, unitOwner, unitType));
+				} else if (line.matches(CONTROL_LINE_REGEX)) {
+					ProvinceIdentifier id = new ProvinceIdentifier(lineScanner.next());
+					Powers controller = Powers.getPowerFromID(lineScanner.next());
+					control.add(new Control(id, controller));
+				} else {
+					throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, POSITION_LINE_REGEX + " or " + CONTROL_LINE_REGEX );
+				}
+			}
+			
+			// We must have a turnID
+			if (turnID == null) {
+				throw new PositionDefinitionException(posFile.getAbsolutePath(), 0, null, "a turn ID in the orders defintion file");
+			}
+
+			// We must have at least one unit definition
+			if (units.isEmpty()) {
+				throw new PositionDefinitionException(posFile.getAbsolutePath(), 0, null, "to find unit positon definitions");
+			}
+			
+			// We must have at least one control definition
+			if (control.isEmpty()) {
+				throw new PositionDefinitionException(posFile.getAbsolutePath(), 0, null, "to find control definitions");
+			}
+		}
+		catch (IOException ioe) {
 			throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + " cannot be read.");
 		}
-		catch (NoSuchElementException nsee){
-			throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + " has problem at line: " + lineNo);
+		catch (NoSuchElementException nsee) {
+			throw new PositionDefinitionException(posFile.getAbsolutePath(), lineNo, line, POSITION_LINE_REGEX + " or " + CONTROL_LINE_REGEX );
 		}
-		catch (YearFormatException nfe){
-			throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + " has problem at line: " + lineNo + ". Expected a turn year.");
-		}
-		catch (SeasonFormatException iae){
-			throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + " has problem at line: " + lineNo + ". Expected a turn season.");
+		finally{
+			try {
+				br.close();
+			}
+			catch (IOException ioe) {
+				//TODO Log exception. Continue anyway: we read the file successfully
+			}
 		}
 
 	}
@@ -142,7 +170,7 @@ public class StandardPositionParser implements IPositionParser{
 			throw new IllegalArgumentException("Position file not specified.");
 		}
 		
-		if (!posFile.isFile()){
+		if (!posFile.isFile()) {
 			throw new IllegalArgumentException("Position file " + posFile.getAbsolutePath() + " cannot be accessed.");
 		}
 	}
