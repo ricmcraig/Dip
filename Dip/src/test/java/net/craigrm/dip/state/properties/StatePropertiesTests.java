e2e2792cc695fcf55bf51c6f261df4890023d3a4
Ric Craig <ricmcraig@gmail.com>
2012-03-12 16:04:25 +0000

    Adds test for state properties

---
+++ Dip/src/test/java/net/craigrm/dip/state/properties/StatePropertiesTests.java
@@ -0,0 +1,11 @@
+package net.craigrm.dip.state.properties;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UnitTypeTest.class })
+public class StatePropertiesTests {
+
+}
---
+++ Dip/src/test/java/net/craigrm/dip/state/properties/UnitTypeTest.java
@@ -0,0 +1,14 @@
+package net.craigrm.dip.state.properties;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTypeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
