0341de220fde3363e165b967e266aa9d4552eb84
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:57:32 +0000

    Updated to reflect renaming of Identifier class

--- Dip/src/main/java/net/craigrm/dip/state/Control.java
+++ Dip/src/main/java/net/craigrm/dip/state/Control.java
@@ -1,18 +1,18 @@
 package net.craigrm.dip.state;
 
-import net.craigrm.dip.map.Identifier;
+import net.craigrm.dip.map.ProvinceIdentifier;
 import net.craigrm.dip.map.properties.Powers;
 
 public class Control {
-	private final Identifier provinceId;
+	private final ProvinceIdentifier provinceId;
 	private final Powers controller;
 
-	public Control(Identifier provinceId, Powers controller){
+	public Control(ProvinceIdentifier provinceId, Powers controller) {
 		this.provinceId = provinceId;
 		this.controller = controller;
 	}
 
-	public Identifier getProvinceId() {
+	public ProvinceIdentifier getProvinceId() {
 		return provinceId;
 	}
 
