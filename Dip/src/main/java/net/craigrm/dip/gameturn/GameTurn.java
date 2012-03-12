455f9cdcefe6de02e01c86371a7dc1b9e1f7bd2f
Ric Craig <ricmcraig@gmail.com>
2012-03-12 15:43:19 +0000

    Adds a few more place holders towards processing a turn.

--- Dip/src/main/java/net/craigrm/dip/gameturn/GameTurn.java
+++ Dip/src/main/java/net/craigrm/dip/gameturn/GameTurn.java
@@ -1,25 +1,36 @@
 package net.craigrm.dip.gameturn;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 import net.craigrm.dip.map.DipMap;
+import net.craigrm.dip.map.IMapDataSource;
+import net.craigrm.dip.orders.Order;
 import net.craigrm.dip.scanners.StandardMapper;
+import net.craigrm.dip.scanners.StandardOrdersParser;
 import net.craigrm.dip.scanners.StandardPositionParser;
 import net.craigrm.dip.state.TurnState;
 
 public class GameTurn {
 
-	private DipMap dm;
-	private TurnState ts;
-// TODO	private Orders orders;
+	private Set<Order> orders;
+	private Map<Order, Outcome> actions;
 	
-	public GameTurn(String mapFile, String positionFile, String ordersFile, String resultsFile){
-		StandardMapper standardMapper = new StandardMapper(mapFile);
-		StandardPositionParser standardPositionParser = new StandardPositionParser(positionFile);
-// TODO		StandardOrdersParser standardOrdersParser = new StandardOrdersParser(ordersFile);
-		dm = new DipMap(standardMapper);
-		ts = new TurnState(standardPositionParser);
+	public GameTurn(String mapFile, String positionFile, String ordersFile, String resultsFile) {
+		IMapDataSource standardMapper = new StandardMapper(mapFile);
+		IPositionDataSource standardPositionParser = new StandardPositionParser(positionFile);
+		IOrdersDataSource standardOrdersParser = new StandardOrdersParser(ordersFile);
+		DipMap.makeMap(standardMapper);
+		TurnState.setTurnState(standardPositionParser);
+		orders = standardOrdersParser.getOrders();
+		actions = new LinkedHashMap<Order, Outcome>();
+		for(Order o: orders) {
+				actions.put(o, new Outcome(o));
+		}
 	}
 	
-	void generateNewPosition(){
+	void generateNewPosition() {
 		
 	}
 }
