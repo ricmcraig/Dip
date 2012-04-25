23c2b3f1ef6c0991316de3c1b9b4864b0048696c
Ric Craig <ricmcraig@gmail.com>
2012-04-25 09:31:16 +0100

    Corrected naming error

--- Dip/src/test/java/net/craigrm/dip/map/properties/OwnerTest.java
+++ Dip/src/test/java/net/craigrm/dip/map/properties/OwnerTest.java
@@ -9,12 +9,12 @@ public class OwnerTest {
 	//Other owners delegated to Powers enum. See PowersTest.
 	
 	private static final String NONE_PARSE_STRING = "";
-	private static final String NONE__REPRESENTATION_STRING = "None";
+	private static final String NONE_REPRESENTATION_STRING = "None";
 
 	@Test
 	public void powerNoneHasCorrectID() {
 		Owner o = new Owner(NONE_PARSE_STRING);
-		assertEquals("None has correct ID", NONE__REPRESENTATION_STRING, o.getPowerID());
+		assertEquals("None has correct ID", NONE_REPRESENTATION_STRING, o.getPowerID());
 	}
 
 	@Test(expected=PowersFormatException.class)
