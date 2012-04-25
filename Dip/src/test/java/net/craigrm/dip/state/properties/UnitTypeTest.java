7259eb8a02b7dbef30df2be8bb4ec5d40e1bef92
Ric Craig <ricmcraig@gmail.com>
2012-04-25 09:35:30 +0100

    Added tests for Unit Type

--- Dip/src/test/java/net/craigrm/dip/state/properties/UnitTypeTest.java
+++ Dip/src/test/java/net/craigrm/dip/state/properties/UnitTypeTest.java
@@ -6,9 +6,33 @@ import org.junit.Test;
 
 public class UnitTypeTest {
 
+	private static final String ARMY_UNIT_TYPE_STRING = "A";
+	private static final String FLEET_UNIT_TYPE_STRING = "F";
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getUnitTypeFromMoveString() {
+		assertEquals("", UnitType.ARMY, UnitType.getType(ARMY_UNIT_TYPE_STRING));
 	}
+
+	@Test
+	public void getUnitTypeFromHoldString() {
+		assertEquals("", UnitType.FLEET, UnitType.getType(FLEET_UNIT_TYPE_STRING));
+	}
+
+	@Test
+	public void getUnitTypeFromMoveStringUpperCase() {
+		assertEquals("", UnitType.ARMY, UnitType.getType(ARMY_UNIT_TYPE_STRING.toUpperCase()));
+	}
+
+	@Test
+	public void getUnitTypeFromMoveStringWhiteSpace() {
+		assertEquals("", UnitType.ARMY, UnitType.getType("  " + ARMY_UNIT_TYPE_STRING + "\t\r\n"));
+	}
+
+	@Test(expected=UnitTypeFormatException.class)
+	public void getUnitTypeFromBadString() {
+		UnitType.getType("X");
+	}
+
 
 }
