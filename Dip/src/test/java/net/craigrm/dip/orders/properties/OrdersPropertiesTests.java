b10f9870e3b8c19245191f6b8f83ce1403ae1c39
Ric Craig <ricmcraig@gmail.com>
2012-04-25 09:33:57 +0100

    Added AdjustmentTypeTest

--- Dip/src/test/java/net/craigrm/dip/orders/properties/OrdersPropertiesTests.java
+++ Dip/src/test/java/net/craigrm/dip/orders/properties/OrdersPropertiesTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ OrderTypeTest.class })
+@SuiteClasses({ AdjustmentTypeTest.class, OrderTypeTest.class })
 public class OrdersPropertiesTests {
 
 }
