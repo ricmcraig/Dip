b412d85690d1d167e8c18d553a9ad1026891e5f1
Ric Craig <ricmcraig@gmail.com>
2012-04-25 09:29:50 +0100

    Added tests for province identifiers, aliases and neighbours. Moved MapperStub to separate class. Moved province making support methods to ProvinceMaker class.

--- Dip/src/test/java/net/craigrm/dip/map/DipMapTest.java
+++ Dip/src/test/java/net/craigrm/dip/map/DipMapTest.java
@@ -2,16 +2,9 @@ package net.craigrm.dip.map;
 
 import static org.junit.Assert.*;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import net.craigrm.dip.map.DipMap;
-import net.craigrm.dip.map.IMapDataSource;
 import net.craigrm.dip.map.ProvinceIdentifier;
 import net.craigrm.dip.map.Province;
-import net.craigrm.dip.map.properties.Owner;
-import net.craigrm.dip.map.properties.Supply;
 import net.craigrm.dip.map.properties.Terrains;
 
 import org.junit.Test;
@@ -19,9 +12,6 @@ import org.junit.Test;
 
 public class DipMapTest {
 	
-	private static final Set<ProvinceIdentifier> EMPTY_ALIASES = Collections.emptySet();
-	private static final Set<ProvinceIdentifier>  EMPTY_NEIGHBOURS = Collections.emptySet();
-
 	@Test
 	public void constructorInitialisesProvinceSets() {
 		DipMap.reloadMap(new MapperStub());
@@ -34,7 +24,7 @@ public class DipMapTest {
 	@Test
 	public void addSingleSeaProvince() {
 		final String PROVINCE_NAME = "ABC";
-		Province sp = makeProvince(Terrains.SEA, PROVINCE_NAME);
+		Province sp = ProvinceMaker.makeProvince(Terrains.SEA, PROVINCE_NAME);
 		DipMap.reloadMap(new MapperStub(sp));
 		DipMap dm = DipMap.getMap();
 		assertTrue("Should be one sea province",dm.getSeaProvinces() != null && dm.getSeaProvinces().size() == 1);
@@ -46,7 +36,7 @@ public class DipMapTest {
 	@Test
 	public void addSingleInlandProvince() {
 		final String PROVINCE_NAME = "ABC";
-		Province ip = makeProvince(Terrains.INLAND, PROVINCE_NAME);
+		Province ip = ProvinceMaker.makeProvince(Terrains.INLAND, PROVINCE_NAME);
 		DipMap.reloadMap(new MapperStub(ip));
 		DipMap dm = DipMap.getMap();
 		assertTrue("Should be no sea province",dm.getSeaProvinces() != null && dm.getSeaProvinces().isEmpty());
@@ -58,7 +48,7 @@ public class DipMapTest {
 	@Test
 	public void addSingleCoastalProvince() {
 		final String PROVINCE_NAME = "ABC";
-		Province cp = makeProvince(Terrains.COAST, PROVINCE_NAME);
+		Province cp = ProvinceMaker.makeProvince(Terrains.COAST, PROVINCE_NAME);
 		DipMap.reloadMap(new MapperStub(cp));
 		DipMap dm = DipMap.getMap();
 		assertTrue("Should be no sea province",dm.getSeaProvinces() != null && dm.getSeaProvinces().isEmpty());
@@ -67,25 +57,93 @@ public class DipMapTest {
 		assertTrue("Coastal province should be the one added", dm.getCoastalProvinces().contains(cp.getIdentifier()));
 	}
 
-	private Province makeProvince(Terrains type, String id) {
-		Province sp = new Province(new ProvinceIdentifier(id), type, Supply.NONE, new Owner(""), "Any Name", EMPTY_ALIASES, EMPTY_NEIGHBOURS);
-		return sp;
+	@Test
+	public void testValidProvinceIdentifiersCorrect() {
+		final String PROVINCE_NAME = "ABC";
+		final String ALIASES = "DEF, GHI";
+		Province cp = ProvinceMaker.makeProvinceWithAliases(Terrains.COAST, PROVINCE_NAME, ALIASES);
+		DipMap.reloadMap(new MapperStub(cp));
+		DipMap dm = DipMap.getMap();
+		assertTrue("Should be valid province",dm.isValidProvinceIdentifier(new ProvinceIdentifier(PROVINCE_NAME)));
+		assertTrue("Should be valid province",dm.isValidProvinceIdentifier(new ProvinceIdentifier("DEF")));
+		assertTrue("Should be valid province",dm.isValidProvinceIdentifier(new ProvinceIdentifier("GHI")));
 	}
-	
-	final class MapperStub implements IMapDataSource {
 
-		Set<Province> stubProvinces = new HashSet<Province>();
-		
-		public MapperStub() {
-		}
-		
-		public MapperStub(Province p) {
-			stubProvinces.add(p);
-		}
-		
-		public Set<Province> getProvinces() {
-			return stubProvinces;
-		}
+	@Test
+	public void testInvalidProvinceIdentifiersCorrect() {
+		final String PROVINCE_NAME = "ABC";
+		final String ALIASES = "DEF, GHI";
+		Province cp = ProvinceMaker.makeProvinceWithAliases(Terrains.COAST, PROVINCE_NAME, ALIASES);
+		DipMap.reloadMap(new MapperStub(cp));
+		DipMap dm = DipMap.getMap();
+		assertFalse("Should be invalid province",dm.isValidProvinceIdentifier(new ProvinceIdentifier("XYZ")));
+	}
+
+	@Test
+	public void testNeighbour() {
+		final String PROVINCE_NAME = "ABC";
+		final String NEIGHBOURS = "DEF, GHI";
+		Province p1 = ProvinceMaker.makeProvinceWithNeighbours(Terrains.COAST, PROVINCE_NAME, NEIGHBOURS);
+		Province p2 = ProvinceMaker.makeProvince(Terrains.COAST, "DEF");
+		Province p3 = ProvinceMaker.makeProvince(Terrains.COAST, "GHI");
+		DipMap.reloadMap(new MapperStub(p1, p2, p3));
+		DipMap dm = DipMap.getMap();
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier("DEF")));
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier("GHI")));
+	}
+
+	@Test
+	public void testAliasWithNeighbours() {
+		final String PROVINCE_NAME = "ABC";
+		final String ALIASES = "BCA, CAB";
+		final String NEIGHBOURS = "DEF";
+		Province p1 = ProvinceMaker.makeProvinceWithAliasesAndNeighbours(Terrains.COAST, PROVINCE_NAME, ALIASES, NEIGHBOURS);
+		Province p2 = ProvinceMaker.makeProvince(Terrains.COAST, "DEF");
+		DipMap.reloadMap(new MapperStub(p1, p2));
+		DipMap dm = DipMap.getMap();
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier("DEF")));
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier("BCA"), new ProvinceIdentifier("DEF")));
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier("CAB"), new ProvinceIdentifier("DEF")));
+	}
+
+	@Test
+	public void testAliasedNeighbours() {
+		final String PROVINCE_NAME = "ABC";
+		final String NEIGHBOURS = "DEF";
+		final String NEIGHBOUR_ALIASES = "EFD, FDE";
+		Province p1 = ProvinceMaker.makeProvinceWithNeighbours(Terrains.COAST, PROVINCE_NAME, NEIGHBOURS);
+		Province p2 = ProvinceMaker.makeProvinceWithAliases(Terrains.COAST, NEIGHBOURS, NEIGHBOUR_ALIASES);
+		DipMap.reloadMap(new MapperStub(p1, p2));
+		DipMap dm = DipMap.getMap();
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier("DEF")));
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier("EFD")));
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier("FDE")));
+	}
+
+	@Test
+	public void testAliasWithAliasedNeighbours() {
+		final String PROVINCE_NAME = "ABC";
+		final String ALIASES = "BCA, CAB";
+		final String NEIGHBOURS = "DEF";
+		final String NEIGHBOUR_ALIASES = "EFD, FDE";
+		Province p1 = ProvinceMaker.makeProvinceWithAliasesAndNeighbours(Terrains.COAST, PROVINCE_NAME, ALIASES, NEIGHBOURS);
+		Province p2 = ProvinceMaker.makeProvinceWithAliases(Terrains.COAST, NEIGHBOURS, NEIGHBOUR_ALIASES);
+		DipMap.reloadMap(new MapperStub(p1, p2));
+		DipMap dm = DipMap.getMap();
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier("BCA"), new ProvinceIdentifier("EFD")));
+		assertTrue("Should be neighbouring province",dm.isNeighbour(new ProvinceIdentifier("CAB"), new ProvinceIdentifier("FDE")));
+	}
+
+	@Test
+	public void testNonNeighbour() {
+		final String PROVINCE_NAME = "ABC";
+		final String NEIGHBOURS = "DEF, GHI";
+		final String NON_NEIGHBOUR = "XYZ";
+		Province p1 = ProvinceMaker.makeProvinceWithNeighbours(Terrains.COAST, PROVINCE_NAME, NEIGHBOURS);
+		Province p2 = ProvinceMaker.makeProvince(Terrains.COAST, NON_NEIGHBOUR);
+		DipMap.reloadMap(new MapperStub(p1, p2));
+		DipMap dm = DipMap.getMap();
+		assertFalse("Should not be neighbouring province",dm.isNeighbour(new ProvinceIdentifier(PROVINCE_NAME), new ProvinceIdentifier(NON_NEIGHBOUR)));
 	}
 
 }
