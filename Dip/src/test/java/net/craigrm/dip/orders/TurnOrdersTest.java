336e102ef7ee662b0a79e24459689d935c83865a
Ric Craig <ricmcraig@gmail.com>
2012-04-25 09:40:34 +0100

    Tests for TurnOrders class

---
+++ Dip/src/test/java/net/craigrm/dip/orders/TurnOrdersTest.java
@@ -0,0 +1,39 @@
+package net.craigrm.dip.orders;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TurnOrdersTest {
+
+	@Test
+	public void testGetTurnIdentifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrders() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAdjustments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrdersByTypeOrderType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrdersByTypeOrderTypeSetOfOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConvoyRoutesForOrder() {
+		fail("Not yet implemented");
+	}
+
+}
